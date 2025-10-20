612663034555ceb8de349ae6a0255f0395bacc01
Brian Santana Gomes Dos Santos <GON0086294@aluno.firjansenaisesi.com.br>
2025-10-20 11:18:55 -0300

    Backgroun, Ground e animacão adicionado

--- Assets/Myproject/Script/NewBehaviourScript.cs
+++ Assets/Myproject/Script/NewBehaviourScript.cs
@@ -13,6 +13,24 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) // 
+    {
+        if (other.gameObject.CompareTag("ground"))
+        Debug.Log("Colidiu");
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("ground"))
+        Debug.Log("Permanece a colisão");
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("ground"))
+        Debug.Log("Saiu da Colisão");
     }
 }
